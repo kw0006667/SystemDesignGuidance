4659dd77e16c2bbf6177c61f77c8d40046f1647d
Tim Chang <kw0006667@hotmail.com>
2026-03-26 04:07:33 -0700

    Fixed repo URL for assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const githubPagesBase = repositoryName ? `/${repositoryName}/` : './';
+
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/BookOfSysmteDesign/' : './',
+  base: process.env.GITHUB_ACTIONS ? githubPagesBase : './',
   resolve: {
     alias: {
       '@content': '/src/content',
